75bb58083999670589c8963b1264c2684698df23
Oratakashi Nhamako <davidcreazy@gmail.com>
2018-09-03 17:01:24 +0700

    Update 3 September

---
+++ Praktikum 2/DefaultValue.java
@@ -0,0 +1,21 @@
+public class DefaultValue{
+    static boolean b;
+    static char c;
+    static byte bt;
+    static short s;
+    static int i;
+    static long l;
+    static float f;
+    static double d;
+
+    public static void main(String[] args) {
+        System.out.println("Default Value b =" + b);
+        System.out.println("Default Value c = " + c);
+        System.out.println("Default Value bt = " + bt);
+        System.out.println("Default Value s = " + s);
+        System.out.println("Default Value i = " + i);
+        System.out.println("Default Value l = " + l);
+        System.out.println("Default Value f = " + f);
+        System.out.println("Default Value d = " + d);
+    }
+}
---
+++ Praktikum 2/DemoPromotion.java
@@ -0,0 +1,24 @@
+public class DemoPromotion{
+    public static void main(String[] args) {
+        byte a = 20;
+        byte b = 40;
+        byte c = 10;
+
+        int total = a*b*c;
+
+        System.out.println(total);
+    }
+}
+/*
+    1. Membuat Class DemoPromotion
+    2. Membuat Method Main
+    3. Mendeklarasikan variabel dengan nama A tipe data Byte dgn nilai 20
+    4.
+    5.
+    6. - 
+    7. Mendeklarasikan Variabel dengan nama total tipe data int dngn nilai
+        a*b*c
+    9. Menampilkan / Mncetak variabel total
+    10. Kurung penutup dari method main
+    11. Kurung Penutup Class
+*/
